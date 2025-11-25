5d719fdcd99e9c4e8f6d59490d183796f614f909
DESKTOP-1RPA4OE\minku <redeye1845@gmail.com>
2025-11-26 00:28:11 +0900

    키 얻어서 작동하게 변경

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const admin = require("firebase-admin");
 const cors = require("cors");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
